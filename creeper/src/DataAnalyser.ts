f8bbdaedbbe67cc839b28b3cbf013ef987af1d4f
Xiuming Xu <xiumingxu@nd.com.cn>
2021-03-25 16:55:53 +0800

    组合设计模式优化代码

---
+++ creeper/src/DataAnalyser.ts
@@ -0,0 +1,44 @@
+import cheerio from 'cheerio'
+import fs from 'fs'
+import { Analyzer } from './crowller'
+interface Course {
+  title: string
+}
+interface courseResult {
+  time: number;
+  data: Course[]
+}
+interface Content {
+  [propName: number]: Course[]  // 1574606086622: [ {title: '1234'}, {title: '1234ddd'} ]
+}
+export default class DataAnalyser implements Analyzer {
+  private getCourseInfo(html: string) {
+    const $ = cheerio.load(html)
+    const courseItem = $('.course-item')
+    const courseInfo: Course[] = [] 
+    courseItem.map((index, ele) => {
+      const descs = $(ele).find('.course-desc')
+      const title = descs.text()
+      courseInfo.push({ title })
+    })
+    return {
+      time: new Date().getTime(),
+      data: courseInfo
+    }
+  }
+
+  generateJsonContent (courseInfo: courseResult, filePath: string) {
+    let fileContent: Content = {}
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    }
+    fileContent[courseInfo.time] = courseInfo.data
+    return fileContent
+  }
+
+  public analyze(html: string, filePath: string) {
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo, filePath)
+    return JSON.stringify(fileContent)
+  }
+}
--- creeper/src/crowller.ts
+++ creeper/src/crowller.ts
@@ -1,60 +1,30 @@
 import superagent from 'superagent'
-import cheerio from 'cheerio'
 import fs from 'fs'
 import path from 'path'
-interface Course {
-  title: string
+import DataAnalyser from './DataAnalyser'
+
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string
 }
-interface courseResult {
-  time: number;
-  data: Course[]
-}
-interface Content {
-  [propName: number]: Course[]  // 1574606086622: [ {title: '1234'}, {title: '1234ddd'} ]
-}
+
 class Crowller {
-  private url = 'http://www.dell-lee.com'
+  private filePath = path.resolve(__dirname, '../data/course.json');
   async getRawHtml() {
     const result = await superagent.get(this.url)
     return result.text
   }
 
-  getCourseInfo(html: string) {
-    const $ = cheerio.load(html)
-    const courseItem = $('.course-item')
-    const courseInfo: Course[] = [] 
-    courseItem.map((index, ele) => {
-      const descs = $(ele).find('.course-desc')
-      const title = descs.text()
-      courseInfo.push({ title })
-    })
-    return {
-      time: new Date().getTime(),
-      data: courseInfo
-    }
-  }
-
-  generateJsonContent (courseInfo: courseResult) {
-    const filePath = path.resolve(__dirname, '../data/course.json')
-    let fileContent: Content = {}
-    if (fs.existsSync(filePath)) {
-      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-    }
-    fileContent[courseInfo.time] = courseInfo.data
-    return fileContent
-  }
-
   async initSpiderProcess() {
-    const filePath = path.resolve(__dirname, '../data/course.json')
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-    const fileContent = this.generateJsonContent(courseInfo)
-    fs.writeFileSync(filePath, JSON.stringify(fileContent))
+    const fileContent = this.analyzer.analyze(html, this.filePath)
+    fs.writeFileSync(this.filePath, fileContent)
   }
 
-  constructor() {
+  constructor(private url: string, private analyzer: Analyzer) {
     this.initSpiderProcess()
   }
 }
 
-const crowller = new Crowller()
+const url = 'http://www.dell-lee.com'
+const analyzer = new DataAnalyser()
+new Crowller(url, analyzer)
